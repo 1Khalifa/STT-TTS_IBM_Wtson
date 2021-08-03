7a2c36470ea7d3c7249e7788bcdd59e99a12dd93
1khalifa <xikhalifa@outlook.com>
2021-08-03 03:52:55 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import speech_recognition as sr
+from ibm_watson import SpeechToTextV1
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+# Text to Speech service credentials
+stt_api = 'uMfXxkvlhlwl2TQMy1nh0mPNSlGNbaX48TeXv2L9zQhs'
+stt_url = 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/1651485d-9eef-46e9-a5ca-9602e269908b'
+
+# Speech to Text service credentials
+tts_api = 'TTrN2keHKeL4yVfTSeZJR8r7sA1Bg3wNZX83UlvpbZjA'
+tts_url = 'https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/5ed483aa-d547-4b98-96eb-59a0de11ca3c'
+
+r = sr.Recognizer()
+speech = sr.Microphone()
+stt_authenticator = IAMAuthenticator(stt_api)
+speech_to_text = SpeechToTextV1(authenticator=stt_authenticator)
+speech_to_text.set_service_url(stt_url)
+
+with speech as source:
+    print("Say Something!\n")
+    audio_file = r.adjust_for_ambient_noise(source)
+    audio_file = r.listen(source)
+    data = speech_to_text.recognize(audio=audio_file.get_wav_data(), content_type='audio/wav').get_result()
+speech_recognition_results = data['results'][0]['alternatives'][0]['transcript']
+print("IBM Watson thinks you said: \n" + speech_recognition_results)
+
+# save STT as txt file
+with open('result.txt', mode='w') as file:
+    file.write(speech_recognition_results)
+print("\nYour speech has been exported to result.txt")
+
+# Insert API Key in place of
+# 'YOUR UNIQUE API KEY'
+tts_authenticator = IAMAuthenticator(tts_api)
+tts = TextToSpeechV1(authenticator=tts_authenticator)
+
+# Insert URL in place of 'API_URL'
+tts.set_service_url(tts_url)
+
+# recognize text using IBM Text to Speech
+# save TTS as mp3 file
+
+f = open("result.txt", "r")
+text = f.read()
+with open('./speech.mp3', 'wb') as audio_file:
+    res = tts.synthesize(text, accept='audio/mp3', voice='en-GB_KateV3Voice').get_result()
+    audio_file.write(res.content)  # write the content to the audio file
+print("Your text has been converted to speech and saved as mp3 file successfully")
